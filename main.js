31f018ef36260d385b6163746ed1e43de648724e
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2020-07-09 00:39:55 +0430

    more code-cleanup

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const fsp = require("fs").promises;
+const { readdir, writeFile } = require("fs").promises;
 const { createCanvas, loadImage } = require('canvas');
-
 const params = require('./params');
 
 const canvas = createCanvas(params.output.width,
 			    params.output.height,
 			    params.output.extension);
 
-const scanStickers = fsp.readdir;
+const scanStickers = readdir;
 
-const createPreview = async (filePath, fileName) => {
-    let ctx = canvas.getContext("2d");
-    let image = await loadImage(filePath);
+async function createPreview (filePath, fileName) {
+    const ctx = canvas.getContext("2d");
+    const image = await loadImage(filePath);
     
     ctx.save();
 
@@ -23,39 +22,37 @@ const createPreview = async (filePath, fileName) => {
     ctx.translate((canvas.width / 2), (canvas.width / 2));
 
     // Create Shadow
-    const cos = Math.cos((params.sticker.shadow.angle) * (Math.PI / 180));
-    const sin = Math.sin((params.sticker.shadow.angle) * (Math.PI / 180));
+    const cos = Math.cos(params.sticker.shadow.angle * Math.PI / 180);
+    const sin = Math.sin(params.sticker.shadow.angle * Math.PI / 180);
     ctx.shadowOffsetX = params.sticker.shadow.offset * cos;
     ctx.shadowOffsetY = params.sticker.shadow.offset * sin;
     ctx.shadowColor = params.sticker.shadow.color;
     ctx.shadowBlur = params.sticker.shadow.blur;
     
     // Draw Image From Center
-    ctx.drawImage(image,
-		  params.sticker.size / -2,
-		  params.sticker.size / -2,
-		  params.sticker.size,
-		  params.sticker.size,
-		 );
+    ctx.drawImage(
+	image,
+	params.sticker.size / -2,
+	params.sticker.size / -2,
+	params.sticker.size,
+	params.sticker.size,
+    );
     ctx.restore();
 
     await downloadFile(fileName);
-};
+}
 
-const addBackgroundToFile = background => {
+async function addBackgroundToFile (background) {
     let ctx = canvas.getContext("2d");
 
     // Draw Background
     ctx.fillStyle = background;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
+    ctx.fillRect(0, 0, canvas.width, canvas.height);    
+}
 
-const downloadFile = async fileName => {
-    return fsp.writeFile(
-	`${params.output.path}/${fileName}`,
-	canvas.toBuffer()
-    );
-};
+async function downloadFile (fileName) {
+    return writeFile(`${params.output.path}/${fileName}`, canvas.toBuffer());
+}
 
 async function main () {
     const stickers = await scanStickers(params.input);
