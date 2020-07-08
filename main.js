d38473d2699ce4148b68557a1aecde55697c818c
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2020-07-09 04:26:41 +0430

    wrap canvas-related functions in `previewGenerator` function

--- main.js
+++ main.js
@@ -6,71 +6,73 @@ const yargs = require('yargs');
 const args = require('./args');
 const rad = require('./rad');
 
-const canvas = createCanvas(args.outputWidth,
-			                args.outputHeight,
-			                args.outputExtension);
+function previewGenerator () {
+    const canvas = createCanvas(args.outputWidth,
+			                    args.outputHeight,
+			                    args.outputExtension);
 
-async function createPreview (filePath, fileName) {
-    const ctx = canvas.getContext('2d');
-    const image = await loadImage(filePath);
+    async function createPreview (filePath, fileName) {
+        const ctx = canvas.getContext('2d');
+        const image = await loadImage(filePath);
 
-    ctx.save();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.save();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    addBackground(ctx, args.background);
+        addBackground(ctx, args.background);
 
-    ctx.translate(canvas.width / 2, canvas.width / 2);
+        ctx.translate(canvas.width / 2, canvas.width / 2);
 
-    createShadow(ctx, args);
-    drawImage(ctx, image, args.stickerSize);
+        createShadow(ctx, args);
+        drawImage(ctx, image, args.stickerSize);
 
-    ctx.restore();
+        ctx.restore();
 
-    await saveBuffer(canvas.toBuffer(), fileName);
-}
+        await saveBuffer(canvas.toBuffer(), fileName);
+    }
 
-function drawImage (ctx, image, size) {
-    ctx.drawImage(image, size / -2, size / -2, size, size);
-}
+    function drawImage (ctx, image, size) {
+        ctx.drawImage(image, size / -2, size / -2, size, size);
+    }
 
-function createShadow (
-    ctx,
-    { shadowAngle, shadowOffset, shadowColor, shadowBlur }
-) {
-    const cos = Math.cos(rad(shadowAngle));
-    const sin = Math.sin(rad(shadowAngle));
-    ctx.shadowOffsetX = shadowOffset * cos;
-    ctx.shadowOffsetY = shadowOffset * sin;
-    ctx.shadowColor = shadowColor;
-    ctx.shadowBlur = shadowBlur;
-}
+    function createShadow (
+        ctx,
+        { shadowAngle, shadowOffset, shadowColor, shadowBlur }
+    ) {
+        const cos = Math.cos(rad(shadowAngle));
+        const sin = Math.sin(rad(shadowAngle));
+        ctx.shadowOffsetX = shadowOffset * cos;
+        ctx.shadowOffsetY = shadowOffset * sin;
+        ctx.shadowColor = shadowColor;
+        ctx.shadowBlur = shadowBlur;
+    }
 
-function addBackground (ctx, background) {
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+    function addBackground (ctx, background) {
+        ctx.fillStyle = background;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 
-async function saveBuffer (buff, fileName) {
-    return writeFile(join(args.output, fileName), buff);
+    async function saveBuffer (buff, fileName) {
+        return writeFile(join(args.output, fileName), buff);
+    }
+
+    return { createPreview,
+             drawImage,
+             createShadow,
+             addBackground,
+             saveBuffer };
 }
 
 async function main () {
+    const pg = previewGenerator();
     const stickers = await readdir(args.input);
 
     for await (const sticker of stickers) {
-	    await createPreview(join(args.input, sticker), sticker);
+	    await pg.createPreview(join(args.input, sticker), sticker);
     }
 }
 
-module.exports = {
-    createPreview,
-    drawImage,
-    createShadow,
-    addBackground,
-    saveBuffer,
-    main,
-};
+module.exports = { main };
 
 if (require.main === module) {
-    main().catch(console.error);
+    main(args).catch(console.error);
 }
