93c5dc883a163c246dba04bbb7eaef51c96c1075
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2020-07-09 01:51:29 +0430

    more code-cleanup!

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const { readdir, writeFile } = require("fs").promises;
+const { readdir, writeFile } = require('fs').promises;
 const { createCanvas, loadImage } = require('canvas');
 const params = require('./params');
 
-const canvas = createCanvas(params.output.width,
-			    params.output.height,
-			    params.output.extension);
+const PI_180 = Math.PI / 180;
+const rad = d => d * PI_180;
 
-const scanStickers = readdir;
+const canvas = createCanvas(params.output.width,
+			                params.output.height,
+			                params.output.extension);
 
 async function createPreview (filePath, fileName) {
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
     const image = await loadImage(filePath);
-    
+
     ctx.save();
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -19,23 +20,23 @@ async function createPreview (filePath, fileName) {
     addBackgroundToFile(params.background);
 
     // Move Pivot Point To Center
-    ctx.translate((canvas.width / 2), (canvas.width / 2));
+    ctx.translate(canvas.width / 2, canvas.width / 2);
 
     // Create Shadow
-    const cos = Math.cos(params.sticker.shadow.angle * Math.PI / 180);
-    const sin = Math.sin(params.sticker.shadow.angle * Math.PI / 180);
+    const cos = Math.cos(rad(params.sticker.shadow.angle));
+    const sin = Math.sin(rad(params.sticker.shadow.angle));
     ctx.shadowOffsetX = params.sticker.shadow.offset * cos;
     ctx.shadowOffsetY = params.sticker.shadow.offset * sin;
     ctx.shadowColor = params.sticker.shadow.color;
     ctx.shadowBlur = params.sticker.shadow.blur;
-    
+
     // Draw Image From Center
     ctx.drawImage(
-	image,
-	params.sticker.size / -2,
-	params.sticker.size / -2,
-	params.sticker.size,
-	params.sticker.size,
+	    image,
+	    params.sticker.size / -2,
+	    params.sticker.size / -2,
+	    params.sticker.size,
+	    params.sticker.size,
     );
     ctx.restore();
 
@@ -43,11 +44,11 @@ async function createPreview (filePath, fileName) {
 }
 
 async function addBackgroundToFile (background) {
-    let ctx = canvas.getContext("2d");
+    let ctx = canvas.getContext('2d');
 
     // Draw Background
     ctx.fillStyle = background;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);    
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 async function downloadFile (fileName) {
@@ -55,10 +56,10 @@ async function downloadFile (fileName) {
 }
 
 async function main () {
-    const stickers = await scanStickers(params.input);
+    const stickers = await readdir(params.input);
 
     for await (const sticker of stickers) {
-	await createPreview(`${params.input}/${sticker}`, sticker);
+	    await createPreview(`${params.input}/${sticker}`, sticker);
     }
 }
 
