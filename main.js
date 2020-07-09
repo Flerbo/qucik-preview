c827c176ca4eb97573c535f0dda48a599df63de9
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2020-07-09 04:30:57 +0430

    move `previewGenerator` to its own module file

--- main.js
+++ main.js
@@ -1,65 +1,7 @@
 const { readdir, writeFile } = require('fs').promises;
-const { createCanvas, loadImage } = require('canvas');
 const { join } = require('path');
-const yargs = require('yargs');
 
-const rad = require('./rad');
-
-function previewGenerator (args) {
-    const canvas = createCanvas(args.outputWidth,
-			                    args.outputHeight,
-			                    args.outputExtension);
-
-    async function createPreview (filePath, fileName) {
-        const ctx = canvas.getContext('2d');
-        const image = await loadImage(filePath);
-
-        ctx.save();
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        addBackground(ctx, args.background);
-
-        ctx.translate(canvas.width / 2, canvas.width / 2);
-
-        createShadow(ctx, args);
-        drawImage(ctx, image, args.stickerSize);
-
-        ctx.restore();
-
-        await saveBuffer(canvas.toBuffer(), fileName);
-    }
-
-    function drawImage (ctx, image, size) {
-        ctx.drawImage(image, size / -2, size / -2, size, size);
-    }
-
-    function createShadow (
-        ctx,
-        { shadowAngle, shadowOffset, shadowColor, shadowBlur }
-    ) {
-        const cos = Math.cos(rad(shadowAngle));
-        const sin = Math.sin(rad(shadowAngle));
-        ctx.shadowOffsetX = shadowOffset * cos;
-        ctx.shadowOffsetY = shadowOffset * sin;
-        ctx.shadowColor = shadowColor;
-        ctx.shadowBlur = shadowBlur;
-    }
-
-    function addBackground (ctx, background) {
-        ctx.fillStyle = background;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    async function saveBuffer (buff, fileName) {
-        return writeFile(join(args.output, fileName), buff);
-    }
-
-    return { createPreview,
-             drawImage,
-             createShadow,
-             addBackground,
-             saveBuffer };
-}
+const previewGenerator = require('./preview-generator');
 
 async function main (args) {
     const pg = previewGenerator(args);
